73636a4be2da4f450382899f25bc13e8953252d1
gayanchandrab <gayanchandrab@gmail.com>
2019-07-28 20:20:27 +0530

    Add related campuses.

--- single-program.php
+++ single-program.php
@@ -84,6 +84,22 @@
             }
           }
         wp_reset_postdata();
+
+        $relatedCampuses = get_field('related_campuses');
+
+        if( $relatedCampuses ){
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">'. get_the_title() .' is available at these campuses:</h2>';
+            echo '<ul class="link-list min-list">';
+
+            foreach( $relatedCampuses as $campus ){ ?>
+                <li><a href="<?php echo get_the_permalink( $campus ); ?>"><?php echo get_the_title( $campus ); ?></a></li>
+            <?php }
+            
+            echo '</ul>';
+        }
+
+
         ?>
     </div>   
 
